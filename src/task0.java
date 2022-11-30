c956308997532cf3eea8786bdfbf10ff87726e6e
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2022-11-30 18:59:32 +0300

    add files into gitignore

---
+++ src/task0.java
@@ -0,0 +1,50 @@
+import com.sun.source.doctree.ParamTree;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class task0 {
+    public static void main(String[] args) {
+
+        Random random = new Random();
+        Scanner input = new Scanner(System.in);
+        int i;
+        int answer = 1;
+        int res = 0;
+        while (answer != 0) {
+
+// add elements in array
+            System.out.print("How much elements do you want to add in your array? ");
+            answer = input.nextInt();
+            int[] array0 = new int[answer];
+
+            for (i = 0; i < array0.length; i++) {
+                array0[i] = random.nextInt(100);
+            }
+            System.out.println(Arrays.toString(array0));
+
+// found a specific element in array
+            System.out.print("which element do you want to found? ");
+            answer = input.nextInt();
+
+            for (int j : array0) {
+                if (j == answer) {
+                    res++;
+                }
+            }
+
+// print result
+            if (res != 0) {
+                System.out.println("array INCLUDE an element " + answer);
+            } else {
+                System.out.println("sorry! but array DOESN'T INCLUDE an element " + answer);
+            }
+
+// exit or continue
+            System.out.println("\nyou can try again - enter 1 " +
+                    "or you can get out - enter 0");
+            answer = input.nextInt();
+        }
+    }
+}
