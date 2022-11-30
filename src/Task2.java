d74ea004dcef681e7e46a12ad09d60fa1eb534ed
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2022-11-30 21:01:05 +0300

    update a task 2

--- src/Task2.java
+++ src/Task2.java
@@ -6,16 +6,13 @@ public class Task2 {
     public static void main(String[] args) {
         Random random = new Random();
         Scanner input = new Scanner(System.in);
-        int answer;
-        int i;
-
 
 // create a first array and print it
         System.out.print("How much elements do you wanna add in your array? ");
-        answer = input.nextInt();
+        int answer = input.nextInt();
         int[] array1 = new int[answer];
 
-        for (i = 0; i < array1.length; i++) {
+        for (int i = 0; i < array1.length; i++) {
             array1[i] = random.nextInt(10);
         }
         System.out.println(Arrays.toString(array1));
