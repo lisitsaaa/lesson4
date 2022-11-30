bec76683b2b61a407a67fefa575a896fbc7ab246
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2022-11-30 20:12:00 +0300

    update a task 1

--- src/task1.java
+++ src/task1.java
@@ -9,6 +9,8 @@ public class task1 {
         int i, j;
         int answer;
         int count = 0;
+
+// create a first array and print it
             System.out.print("How much elements do you wanna add in your array? ");
             answer = input.nextInt();
             int[] array1 = new int[answer];
@@ -18,6 +20,7 @@ public class task1 {
             }
             System.out.println(Arrays.toString(array1));
 
+// remove an element
             System.out.print("which element do you want to remove? ");
             answer = input.nextInt();
 
@@ -27,6 +30,12 @@ public class task1 {
                 }
             }
 
+// print a current status
+        if (count != 0) {
+            System.out.println("array INCLUDE an element " + answer
+                    + "\nupdate array: ");
+
+// create a second array and print result
             int[] array2 = new int[array1.length - count];
 
             for (i = 0, j = 0; i < array1.length; i++) {
@@ -37,7 +46,11 @@ public class task1 {
                     j++;
                 }
             }
-
             System.out.println(Arrays.toString(array2));
+        } else {
+            System.out.println("sorry! but array DOESN'T INCLUDE an element " + answer
+                    + "\nwe have an old array: " + Arrays.toString(array1));
+
+        }
     }
 }
